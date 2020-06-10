eb2a69bc90e2166977f43630e4afd18a5fa01740
wuyongzhi <srdrm@icloud.com>
2020-06-10 23:50:50 +0800

    performance issue sample

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+
+        <c-input v-model="title"/>
+        <c-button @click="toggle = !toggle">toggle components</c-button>
+
+        <CFlex v-if="toggle">
+            <CBox pt="4" ma="5">
+                <CBox>Hello, every body</CBox>
+
+                <CStack>
+                    <CBox v-for="(n) in 100" :key="'cbox' + n">{{n}}
+                        <c-input v-model="title" v-if="n===0"/>
+                    </CBox>
+                </CStack>
+                <CFlex></CFlex>
+            </CBox>
+        </CFlex>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import {CBox, CButton, CFlex, CInput} from '@chakra-ui/vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        data() {
+            return {
+                toggle: false,
+                title: "",
+                title2: ""
+            }
+        },
+        components: {
+            CBox, CButton, CFlex, CInput
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Chakra, {CReset, CThemeProvider} from '@chakra-ui/vue'
 
 Vue.config.productionTip = false
+Vue.use(Chakra)
+
+// Vue.component(CBox.name, CBox)
+// Vue.component(CFlex.name, CFlex)
+// Vue.component(CInput.name, CInput)
+// Vue.component(CButton.name, CButton)
+// Vue.component(CCloseButton.name, CCloseButton)
+// Vue.component(CStack.name, CStack)
+// Vue.component(CStat.name, CStat)
+
 
 new Vue({
-  render: h => h(App),
+    render: (h) => h(CThemeProvider, [h(CReset), h(App)])
 }).$mount('#app')
